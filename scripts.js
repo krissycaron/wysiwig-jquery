5048464fb9801357b114d1b7b068c63da0e8e66f
Kristin Caron <krissycaron@gmail.com>
2017-04-07 13:39:43 -0500

    getting stuck on keppress after focus

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-// var input = document.getElementById("user-input");
+var input = document.getElementById("user-input");
 var data;
 
 
@@ -20,7 +20,7 @@ function doSomething(data){
 	var card = "";
 		card += `<div id="person-${i}" class="person"><header class="child">`;
 		card += `<h3 class="grandChild">${data.people[i].name}</h3><h6 class="grandChild">${data.people[i].title}</h6></header>`;
-		card += `<section class="child"><p class="grandChild">${data.people[i].bio}</p><img class="grandChild" src="${data.people[i].image}"></section>`;
+		card += `<section class="child"><p class="bioClass">${data.people[i].bio}</p><img class="grandChild" src="${data.people[i].image}"></section>`;
 		card += `<footer class="child"><p class="grandChild">${data.people[i].lifespan.birth} to ${data.people[i].lifespan.death}</p></footer>`;
 		card +=`</div>`;
 	$("#peopleContainer").append(card);
@@ -32,8 +32,30 @@ function doSomething(data){
 // 	$("#user-input").focus();
 // 	});
 // }
-function focusingInput(){
- ("body").click(function(){
-   ("#user-input").focus();
- });
-}
+// function (){
+ $("#peopleContainer").click(function(data){
+ 		var clickedBio = data.target;
+ 	if (clickedBio.className === "bioClass"){
+   		input.focus();
+   		input.val(clickedBio.innerHTMl);
+   		input.keydown(function(){
+   			clickedBio.innerHTMl = input.val();
+   			console.log("keypress");
+   		});
+
+   	}
+
+ }); 
+
+
+
+
+
+
+
+
+
+
+
+
+
